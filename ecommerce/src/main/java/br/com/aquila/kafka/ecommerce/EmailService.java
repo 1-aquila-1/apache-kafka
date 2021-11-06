462afe1a206c3d381b3bb23f069def682e1f773f
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-11-06 13:40:22 -0300

    Refatoração

--- ecommerce/src/main/java/br/com/aquila/kafka/ecommerce/EmailService.java
+++ ecommerce/src/main/java/br/com/aquila/kafka/ecommerce/EmailService.java
@@ -1,44 +1,30 @@
 package br.com.aquila.kafka.ecommerce;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class EmailService {
+
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList(Topic.ECOMMERCE_SEND_EMAIL.name()));
-        while (true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                for (var record : records) {
-                    System.out.println("-----------------------------------------");
-                    System.out.println("Processing new email");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        // ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("email processed");
-                }
-            }
+        var emailService = new EmailService();
+        try (var service = new KafkaService(EmailService.class.getSimpleName(), Topic.ECOMMERCE_SEND_EMAIL.name(),
+                emailService::parse)) {
+            service.run();
         }
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-        return properties;
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Send email");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Email sent");
     }
 }
